772c1a30b3f96faf9b731a62e93afba6879a3b7b
Caridad Rivera <ccrive10@gmail.com>
2020-11-11 21:50:47 -0500

    root route ready to go

---
+++ db/migrate/20201111193835_create_entries.rb
@@ -0,0 +1,14 @@
+class CreateEntries < ActiveRecord::Migration[5.2]
+  def change
+    create_table :entries do |t|
+      t.string :meal_type
+      t.integer :calories
+      t.integer :proteins
+      t.integer :portions
+      t.integer :carbs
+      t.integer :fats
+
+      t.timestamps
+    end
+  end
+end
